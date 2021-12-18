5845da8a83053b2577bc4dd1f09489fdf7785349
Paul Guillermo Ottenwalder <paulguillermo19@gmail.com>
2021-12-18 11:15:10 -0400

    add json-server and install axios

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 import { ApolloServer, gql, UserInputError } from "apollo-server"
 import {v1 as uuid} from "uuid"
-
-const persons = [
-    {
-        name: 'Paul',
-        phone: '8093555339',
-        street: "Av. Francisco Lora",
-        city: "Santiago",
-        id: "3D55FE6E05CB645336F7E86D8904D471"
-    },
-    {
-        name: 'Raine',
-        phone: '8097894561',
-        street: "Yapur Dumit",
-        city: "Santiago",
-        id: "62FB18674B6DBD51134BE17A155C5CD3"
-    },
-    {
-        name: 'Miguel',
-        street: "Av. Circunvalacion Sur",
-        city: "Santiago",
-        id: "4C1F31967FDC91AAC1757486354D9C78"
-    },
-]
+import axios from "axios"
 
 const typeDefs = gql`
 
@@ -56,26 +34,39 @@ const typeDefs = gql`
             street: String!,
             city: String!
         ): Person
+        editPhoneNumber(
+            name: String!,
+            phone: String!
+        ): Person
     }
 `
 
+const personsFromApi = async () => {
+    return await axios.get('http://localhost:3000/persons')
+}
+
 const resolvers = {
     Query: {
-        personCount: () => persons.length,
-        allPersons: (root, args) => {
+        personCount: async () => {
+            const {data: persons} = await personsFromApi()
+            return persons.length
+        },
+        allPersons: async (root, args) => {
+            const {data: persons} = await personsFromApi()
             if (!args.phone) return persons
-
             return persons.filter(person => (
                 args.phone === "YES" ? person.phone : !person.phone
             ))
         },
-        findPerson: (root, args) => {
+        findPerson: async (root, args) => {
+            const {data: persons} = await personsFromApi()
             const {name} = args
             return persons.find(person => person.name === name)
         }
     },
     Mutation: {
-        addPerson: ( root, args ) => {
+        addPerson: async ( root, args ) => {
+            const {data: persons} = await personsFromApi()
             if(persons.find(person => person.name === args.name)){
                 throw new UserInputError("Person already exist!", {
                     invalidArgs: args.name
@@ -84,6 +75,16 @@ const resolvers = {
             const person = {...args, id: uuid()}
             persons.push(person)
             return person
+        },
+        editPhoneNumber: async(root, args) => {
+            const {data: persons} = await personsFromApi()
+            const personIndex = persons.findIndex(person => person.name === args.name)
+            if (personIndex === -1){
+                throw new UserInputError("Person no exists!")
+            }
+            const updatedPerson = {...persons[personIndex], phone: args.phone}
+            persons[personIndex] = updatedPerson
+            return updatedPerson
         }
     },
     Person: {
